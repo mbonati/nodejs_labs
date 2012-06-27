8392ffabc28ae55f368bd852146b5ab732836a58
Marco Bonati <marco.bonati@gmail.com>
2012-06-27 14:08:24 +0200

    moved files

---
+++ src/server/simpleServerApp.js
@@ -0,0 +1,12 @@
+console.log('Starting simple server...');
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
+
+
