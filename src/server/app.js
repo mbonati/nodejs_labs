fa55b281868eb2f2190b7fa0ba111a8cb1d52644
Marco Bonati <marco.bonati@gmail.com>
2012-06-28 12:27:03 +0200

    added new system call test

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,11 @@
 var server = require("./server");
 var router = require("./route");
+var exec = require("child_process").exec;
+
+exec("ls -la", function (error, stdout, stderr) {
+	console.log("Listing files...");
+    console.log(stdout);
+});
 
 server.start(router.route);
 
